5de9b5dd2d58b3fc5b3c60c6246388eaf0fc51ca
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-23 00:45:50 +0900

    fix js

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const endpoint = "https://script.google.com/macros/s/AKfycbzZtvOvf14TaMdRIYzocRcf3mktzGgXvlFvyczo/exec";
-$.ajax({
-    type: 'GET',
-    url: endpoint,
-    dataType: 'jsonp',
-		jsonp: 'callback',
-    jsonpCallback: 'jsonpTestCallback',
-    data: {
-        text: 'Hello'
-    },
-    success: response => {
-        alert("OK");
-    }
+
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+  $.ajax({
+      type: 'GET',
+      url: endpoint,
+      dataType: 'jsonp',
+  		jsonp: 'callback',
+      jsonpCallback: 'jsonpTestCallback',
+      data: {
+          text: 'Hello'
+      },
+      success: response => {
+          alert("OK");
+      }
+  });
 });
