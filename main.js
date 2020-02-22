75733f5ff623f071eff6ab4a0a2791ca144b4d4f
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 18:57:20 +0900

    change js

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const after = document.getElementById('after');
 // ボタンをクリックしたらイベント発動
 button.addEventListener('click', () => {
     fetch("https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja") // APIのURL
-    .then(response => {
-        return response.json();
+    .then(function(result) => {
+        return result.json();
     })
-    .then(Json => {
-        // imgタグに取得したURL画像を入れる
-        after.innerText = Json["text"];
+    .then(function(json) => {
+        after.innerText = json["text"];
         before.innerText = "変換済み";
         button.innerText = "受信済み";
     });
