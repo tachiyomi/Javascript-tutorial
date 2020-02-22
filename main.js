f635a0116bf2cbfc9c01df6b22fbabcf1cc15642
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 21:25:00 +0900

    add test code

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const before = document.getElementById('before');
 const after = document.getElementById('after');
 // ボタンをクリックしたらイベント発動
 button.addEventListener('click', () => {
+  fetch('https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja')
+  .then(function(response){
+    if (response.ok) {
+        console.log(response.url); //レスポンスのURL
+        console.log(response.status); //レスポンスのHTTPステータスコード
+    }
+  });
+  /*
     fetch('https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja') // APIのURL
     .then(function(response) => {
         return response.json();
@@ -13,4 +21,5 @@ button.addEventListener('click', () => {
         button.innerText = "受信済み";
     });
     button.innerText = "受信なし";
+  */
 });
