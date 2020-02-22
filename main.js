d15b6f7b41cc78d2224b0b9bc4b74ff7da99c761
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 19:02:46 +0900

    update js

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const before = document.getElementById('before');
 const after = document.getElementById('after');
 // ボタンをクリックしたらイベント発動
 button.addEventListener('click', () => {
-    fetch("https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja") // APIのURL
-    .then(function(result) => {
-        return result.json();
+    fetch('https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja') // APIのURL
+    .then(function(response) => {
+        return response.json();
     })
-    .then(function(json) => {
-        after.innerText = json["text"];
+    .then(function(myJson){
+        after.innerText = myJson["text"];
         before.innerText = "変換済み";
         button.innerText = "受信済み";
     });
