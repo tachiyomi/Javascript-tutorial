26c05b06233b21cc281a60f9f22e5ddc874a1ea6
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 16:23:44 +0900

    update js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function over(){
-  document.getElementsByTagName('p')[0].visibility = "visible";
-  document.getElementsByTagName('img')[0].opacity = 0.6;
+function over(doc){
+  doc.getElementsByTagName('p')[0].visibility = "visible";
+  doc.getElementsByTagName('img')[0].opacity = 0.6;
 
 }
 
-function out(){
-  document.getElementsByTagName('p')[0].visibility = "hidden";
-  document.getElementsByTagName('img')[0].opacity = 1.0;
+function out(doc){
+  doc.getElementsByTagName('p')[0].visibility = "hidden";
+  doc.getElementsByTagName('img')[0].opacity = 1.0;
 }
