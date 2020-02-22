a2531277f4f9ecd8fbc90997d7382da48e23d36e
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 18:14:43 +0900

    add gas js

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ function out(doc){
   doc.getElementsByTagName('p')[0].style.visibility = "hidden";
   doc.getElementsByTagName('img')[0].style.opacity = 1.0;
 }
+
+const button = document.getElementById('btn');
+const before = document.getElementById('before');
+const after = document.getElementById('after');
+// ボタンをクリックしたらイベント発動
+button.addEventListener('click', () => {
+    fetch('https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=こんにちわ&source=ja&target=en') // APIのURL
+    .then(response => {
+        return response.json();
+    })
+    .then(Json => {
+        // imgタグに取得したURL画像を入れる
+        after.innerText = Json['text'];
+    });
+});
