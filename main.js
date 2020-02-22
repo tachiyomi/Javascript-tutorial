909a2b5271d56ce04c123afe5a9fce956246db23
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 18:36:49 +0900

    update css

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const before = document.getElementById('before');
 const after = document.getElementById('after');
 // ボタンをクリックしたらイベント発動
 button.addEventListener('click', () => {
-    fetch('https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=こんにちわ&source=ja&target=en') // APIのURL
+    fetch("https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=こんにちわ&source=ja&target=en") // APIのURL
     .then(response => {
         return response.json();
     })
     .then(Json => {
         // imgタグに取得したURL画像を入れる
-        after.innerText = Json['text'];
+        after.innerText = Json["text"];
         button.innerText = "受信済み";
     });
     button.innerText = "受信なし";
