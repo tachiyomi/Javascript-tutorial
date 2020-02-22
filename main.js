2d1661e51d1a994bee2aed2f6760773959140705
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-22 18:48:01 +0900

    change url

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const before = document.getElementById('before');
 const after = document.getElementById('after');
 // ボタンをクリックしたらイベント発動
 button.addEventListener('click', () => {
-    fetch("https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=こんにちわ&source=ja&target=en") // APIのURL
+    fetch("https://script.google.com/macros/s/AKfycbxvNsIJiFYAaZJLMX9zWGlOWMa-LUaBKHBKoqf_6eP3Ie_XUq0/exec?text=Hello&source=en&target=ja") // APIのURL
     .then(response => {
         return response.json();
     })
