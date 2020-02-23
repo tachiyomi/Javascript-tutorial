59f6eafcd9dfbaa21db1bb76def14fd55b3bc07c
立ち読み <naonosuke311@yahoo.co.jp>
2020-02-23 15:21:32 +0900

    update js

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function out(doc){
   const button = document.getElementById('btn');
 
 function translation(){
-  const beforeOption = document.getElementById('before').getElementsByTagName('select').selectedIndex;
-  const afterOption = document.getElementById('after').getElementsByTagName('select').selectedIndex;
+  const beforeOption = 0;//document.getElementById('before').getElementsByTagName('select').selectedIndex;
+  const afterOption = 1;//document.getElementById('after').getElementsByTagName('select').selectedIndex;
 
   $.ajax({
       type: 'GET',
